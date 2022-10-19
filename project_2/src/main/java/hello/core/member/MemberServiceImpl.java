05001fd627034e65da34de1ac28953092cf9993d
ebcode2021 <ebcode2021@gmail.com>
2022-10-19 11:26:57 +0900

    loss commit

--- project_2/src/main/java/hello/core/member/MemberServiceImpl.java
+++ project_2/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    private final MemberRepository memberRepository;
+    // 생성자를 통해서 MemberRepository에 뭐가 들어갈지 보게 된다.
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
