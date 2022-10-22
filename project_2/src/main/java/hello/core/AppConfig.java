aef6ecc8424267f5f613e2a0969b7907a062576e
ebcode2021 <ebcode2021@gmail.com>
2022-10-22 16:13:23 +0900

    java to spring f

--- project_2/src/main/java/hello/core/AppConfig.java
+++ project_2/src/main/java/hello/core/AppConfig.java
@@ -8,18 +8,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
-public class AppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig { // application의 구성정보. 설정정보.
+    @Bean
     public MemberService memberService() {
+
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+
+    @Bean
     public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- project_2/src/main/java/hello/core/MemberApp.java
+++ project_2/src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
         //MemberService memberService = new MemberServiceImpl();
+
+        //Appconfig에 있는 정보를 갖고, @Bean 들을 컨테이너에 넣어준다. 이제는 스프링 컨테이너를 통해서 가져온다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
