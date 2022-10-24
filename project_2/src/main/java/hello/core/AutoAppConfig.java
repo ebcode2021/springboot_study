7f4b4c0ed2f373eca7abd75eb64d2786956e75e6
ebcode2021 <ebcode2021@gmail.com>
2022-10-24 14:19:55 +0900

    component scan

---
+++ project_2/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan ( // @component 붙은 것을 스프링 빈으로 등록
+        basePackages = "hello.core",
+        excludeFilters=@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemoryMemberRepository memoryMemberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
--- project_2/src/main/java/hello/core/member/MemberServiceImpl.java
+++ project_2/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,11 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     private final MemberRepository memberRepository;
     // 생성자를 통해서 MemberRepository에 뭐가 들어갈지 보게 된다.
+    @Autowired // 생성자에 붙여주기 (ac.getBean(MemberRepository.class))
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
