16ed7680381cbba27c2d88805bdae1d8ba055d93
Eunbi Son <ebcode2021@gmail.com>
2022-11-06 21:35:02 +0900

    add writer

--- servlet/src/main/java/hello/servlet/web/servlet/MemberListServlet.java
+++ servlet/src/main/java/hello/servlet/web/servlet/MemberListServlet.java
@@ -49,6 +49,7 @@ public class MemberListServlet extends HttpServlet {
             w.write(" <td>" + member.getAge() + "</td>");
             w.write(" </tr>");
         }
+        
         w.write(" </tbody>");
         w.write("</table>");
         w.write("</body>");
