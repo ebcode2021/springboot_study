548f74a38355583106aa5f817ee28be9831ae909
Eunbi Son <ebcode2021@gmail.com>
2022-11-11 15:28:04 +0900

    logging study

---
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,29 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+    //private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest(){
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+        log.trace("trace = {}", name);
+        log.debug("debug log = {}", name);
+        log.info("info log = {}", name);
+        log.warn("warn log = {}", name);
+        log.error("error log = {}, name");
+
+        // @RestController 함수에서 문자열을 return하면
+        // http message body에 data를 넣어 화면에 바로 보여준다.
+        return "ok";
+    }
+}
